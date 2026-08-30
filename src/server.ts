16c4257603423d57e3b6e90c16d775eb3a216961
Thiago <comsalmanteiga49@gmail.com>
2026-08-30 15:56:32 -0300

    chore: express listen

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config'
 
+import app from './app.js'
 import { initDatabase } from './databases/database.js'
+
+const PORT = Number(process.env.PORT) || 3000
 
 async function server() {
   try {
     await initDatabase()
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta: ${String(PORT)}`)
+    })
   } catch (error) {
     console.error('Erro ao iniciar a aplicação:', error)
     process.exit(1)
